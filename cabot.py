05c231ed479f453cdbde7c2b2d6239df96ad5b0a
Raziel Willms <42423628+RazielWillms@users.noreply.github.com>
2023-02-23 11:07:12 -0300

    Update cabot.py

--- cabot.py
+++ cabot.py
@@ -1,63 +1,80 @@
-#Cabot- robô acadêmico para puxar notas no SIGA- Faculdade Uníntese- Raziel Haas Willms
+# Cabot- robô acadêmico para puxar notas no SIGA- Faculdade Uníntese- Raziel Haas Willms
 import pyautogui
 import time
-import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
+#automatiza a atualização do webdriver, do contrário seria necessário instalação manual a cada atualização do chrome
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
 
 navegador.get('https://unintese.sistemasiga.net/login')
 
-#xpaths para chegarmos na área de lançamento de notas
-navegador.find_element('xpath','/html/body/div[2]/form[1]/div[1]/div/div/input').send_keys('Login')
-navegador.find_element('xpath','/html/body/div[2]/form[1]/div[2]/div/div/input').send_keys('Senha')
-navegador.find_element('xpath','/html/body/div[2]/form[1]/div[3]/div/div/select').send_keys('Administração')
-navegador.find_element('xpath','//*[@id="login-btn"]/i').click()
-navegador.find_element('xpath','//*[@id="noprint"]/li[20]/a').click()
+# xpaths para chegarmos na área de lançamento de notas
+navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[1]/div/div/input').send_keys('04853343059')
+navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[2]/div/div/input').send_keys('04853343059')
+navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[3]/div/div/select').send_keys('Administração')
+navegador.find_element('xpath', '//*[@id="login-btn"]/i').click()
+navegador.find_element('xpath', '//*[@id="noprint"]/li[20]/a').click()
 time.sleep(2)
-navegador.find_element('xpath','//*[@id="noprint"]/li[20]/ul/li[2]/a').click()
+navegador.find_element('xpath', '//*[@id="noprint"]/li[20]/ul/li[2]/a').click()
 
-
-
-
-a=open('cursos.txt','a') 
-#a=open('turmas.txt','a') 
-#a=open('disciplinas.txt','a') 
-navegador.find_element('xpath','//*[@id="curso_chosen"]/a/span').click()
-z= navegador.find_element('xpath','/html/body/div[3]/div[2]/div/form/fieldset/div[1]/div/div/div/ul')
-print(z)
+#extração do texto do elemento Cursos
+navegador.find_element('xpath', '//*[@id="curso_chosen"]/a').click()
+z = navegador.find_element('class name', 'chosen-results').get_attribute("innerText")
+a = open('cursos.txt', 'a')
+a.write(z)
 a.close()
 
-#devem retornar os ID´s que ainda não foram utilizados, estudar selenium para tentar gerar o arquivo a partir do formulário exibido no SIGA
-def ler_cursos():
-    z = open('cursos.txt','r+')
-    cursoid=z.readlines()
-    print(cursoid)
-    cursoid=[x.strip('\n') for x in cursoid]
-    print(cursoid)
-    z.close()
-    for linha in cursoid:
-        print(linha)
+#criação de arquivos base para vetorização dos cursos, turmas e disciplinas
+#a = open('cursos.txt', 'a')
+#a = open('turmas.txt','a')
+#a = open('disciplinas.txt','a')
+#a.close()
+
+# Devem retornar vetores com todas as possibilidades de curso, turma e disciplina
+def ler_cursos(cursoid=None):
+    #a = open('cursos.txt', 'r+')
+    #with open("cursos.txt", "r") as f:
+    #    string = f.read()
+    #a.write(string)
+    #cursoid = a.readlines()
+    #cursoid = [x.strip('\n') for x in cursoid]
+    #for linha in cursoid:
+    #    print(linha)
+    #    print('próximo valor na lista')
+    #a.close()
+    #with open(a) as f:
+    #    for x, linha in enumerate(f):
+    #        if x == 3:
+    #            print('numero')
+    #            print(enumerate)
+    #            print('linha')
+    #            print(linha)
+    #            print('x')
+    #            print(x)
+    #            print('f')
+    #            print(f)
     return cursoid
 
-def ler_turmas():
-    z = open('turmas.txt','r+')
-    return turmaid
 
-def ler_disciplinas():
-    z = open('disciplinas.txt','r+')
-    return disciplinaid
+#def ler_turmas(turmaid=None):
+#    a = open('turmas.txt', 'r+')
+#    return turmaid
 
 
-x=ler_cursos()
+#def ler_disciplinas(disciplinaid=None):
+#    a = open('disciplinas.txt', 'r+')
+#    return disciplinaid
 
-#trocar coordenadas por identificação do campo a ser selecionado/preebchido
-pyautogui.click(1291, 288)
-pyautogui.write(x)
-time.sleep(1)
-pyautogui.press('enter')
+
+#ler_cursos()
+
+# trocar coordenadas por identificação do campo a ser selecionado/preebchido
+#pyautogui.click(1291, 288)
+#pyautogui.write(x)
+#time.sleep(1)
+#pyautogui.press('enter')
 
 time.sleep(50)
