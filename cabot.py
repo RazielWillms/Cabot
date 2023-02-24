9d17ba867b31579138dfc5d6ff4d5d0c34effc0c
Raziel Willms <42423628+RazielWillms@users.noreply.github.com>
2023-02-24 09:30:18 -0300

    Update cabot.py
    devo continuar o estudo de selenium, especificamente manipulação de form dropdown para continuar a lógica

--- cabot.py
+++ cabot.py
@@ -5,76 +5,63 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-#automatiza a atualização do webdriver, do contrário seria necessário instalação manual a cada atualização do chrome
+# automatiza a atualização do webdriver, do contrário seria necessário instalação manual a cada atualização do chrome
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
 
 navegador.get('https://unintese.sistemasiga.net/login')
 
 # xpaths para chegarmos na área de lançamento de notas
-navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[1]/div/div/input').send_keys('Login')
-navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[2]/div/div/input').send_keys('Senha')
+navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[1]/div/div/input').send_keys('04853343059')
+navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[2]/div/div/input').send_keys('04853343059')
 navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[3]/div/div/select').send_keys('Administração')
 navegador.find_element('xpath', '//*[@id="login-btn"]/i').click()
 navegador.find_element('xpath', '//*[@id="noprint"]/li[20]/a').click()
 time.sleep(2)
 navegador.find_element('xpath', '//*[@id="noprint"]/li[20]/ul/li[2]/a').click()
 
-#extração do texto do elemento Cursos
+# extração do texto do elemento Cursos
 navegador.find_element('xpath', '//*[@id="curso_chosen"]/a').click()
-z = navegador.find_element('class name', 'chosen-results').get_attribute("innerText")
-a = open('cursos.txt', 'a')
-a.write(z)
+elementocurso = navegador.find_element('class name', 'chosen-results').get_attribute("innerText")
+a = open('cursos.txt', 'w')
+a.write(elementocurso)
 a.close()
 
-#criação de arquivos base para vetorização dos cursos, turmas e disciplinas
-#a = open('cursos.txt', 'a')
-#a = open('turmas.txt','a')
-#a = open('disciplinas.txt','a')
-#a.close()
+# Lógica de 'puxada de nota'
+curso = open('cursos.txt', 'r')
+cursoid = curso.readlines()
+contadorc = 0
+contadort = 0
+contadord = 0
 
-# Devem retornar vetores com todas as possibilidades de curso, turma e disciplina
-def ler_cursos(cursoid=None):
-    #a = open('cursos.txt', 'r+')
-    #with open("cursos.txt", "r") as f:
-    #    string = f.read()
-    #a.write(string)
-    #cursoid = a.readlines()
-    #cursoid = [x.strip('\n') for x in cursoid]
-    #for linha in cursoid:
-    #    print(linha)
-    #    print('próximo valor na lista')
-    #a.close()
-    #with open(a) as f:
-    #    for x, linha in enumerate(f):
-    #        if x == 3:
-    #            print('numero')
-    #            print(enumerate)
-    #            print('linha')
-    #            print(linha)
-    #            print('x')
-    #            print(x)
-    #            print('f')
-    #            print(f)
-    return cursoid
+for x in cursoid:
+    contadorc = contadorc+1
+    navegador.find_element('xpath', '//*[@id="curso_chosen"]/a').click()
+    #navegador.find_element('xpath','//*[@id="curso_chosen"]/div/div/input').send_keys(cursoid[contadorc])
+    pyautogui.write(cursoid[contadorc])
+    time.sleep(2)
+    pyautogui.press('enter')
+    time.sleep(5)
 
-
-#def ler_turmas(turmaid=None):
-#    a = open('turmas.txt', 'r+')
-#    return turmaid
-
-
-#def ler_disciplinas(disciplinaid=None):
-#    a = open('disciplinas.txt', 'r+')
-#    return disciplinaid
-
-
-#ler_cursos()
-
-# trocar coordenadas por identificação do campo a ser selecionado/preebchido
-#pyautogui.click(1291, 288)
-#pyautogui.write(x)
-#time.sleep(1)
-#pyautogui.press('enter')
-
-time.sleep(50)
+    # extração do texto do elemento turmas
+    navegador.find_element('xpath', '//*[@id="turma_chosen"]/a').click()
+    elementoturma = navegador.find_element('xpath', '//*[@id="turma_chosen"]/div/ul').get_attribute("innerText")
+    b = open('turmas.txt', 'w')
+    b.write(elementoturma)
+    b.close()
+    time.sleep(1)
+    turma = open('turmas.txt', 'r')
+    turmaid = turma.readlines()
+    # inserir lógica para remoção das sentenças que denominam se a turma é ativa ou não
+    for y in turmaid:
+        time.sleep(1)
+        contadort = contadort+1
+        navegador.find_element('xpath', '//*[@id="turma_chosen"]/a').click()
+        #navegador.find_element('xpath', '//*[@id="turma_chosen"]/div/div/input').send_keys(turmaid[contadort])
+        pyautogui.write(turmaid[contadort])
+        time.sleep(2)
+        pyautogui.press('enter')
+        time.sleep(500)
+else:
+    print('nenhum curso cadastrado')
+    time.sleep(500)
