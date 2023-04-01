e635e6c73784f987f64bc817b7eea37c636f8a8e
Raziel Willms <42423628+RazielWillms@users.noreply.github.com>
2023-04-01 10:54:13 -0300

    Update cabot.py
    Ciclo de 'puxada' finalizado, o robô já é capaz de verificar cursos, turmas, disciplinas e importar as notas do Moodle, além de inverter as marcações de envio para realização de novo cálculo. É necessário testar para tratar os erros.
    Lista de problemas atuais e/ou implementação necessária:
    1 Resolvido;
    2 Resolvido;
    3 Resolvido;
    4 Não registra o andamento, em caso de erro no meio do processo seria necessário realizar toda a operação novamente;
    5 Precisa verificar quantos alunos a grid listou e setar todas as flags como marcadas.

--- cabot.py
+++ cabot.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.keys import Keys
 
 # automatiza a atualização do webdriver, do contrário seria necessário instalação manual a cada atualização do chrome
 servico = Service(ChromeDriverManager().install())
@@ -14,8 +15,8 @@ navegador = webdriver.Chrome(service=servico)
 # xpaths para chegarmos na área de lançamento de notas
 navegador.maximize_window()
 navegador.get('https://unintese.sistemasiga.net/login')
-navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[1]/div/div/input').send_keys('Login')
-navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[2]/div/div/input').send_keys('Senha')
+navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[1]/div/div/input').send_keys('04853343059')
+navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[2]/div/div/input').send_keys('04853343059')
 navegador.find_element('xpath', '/html/body/div[2]/form[1]/div[3]/div/div/select').send_keys('Administração')
 navegador.find_element('xpath', '//*[@id="login-btn"]/i').click()
 navegador.find_element('xpath', '//*[@id="noprint"]/li[20]/a').click()
@@ -33,67 +34,77 @@ a.close()
 
 # Lógica de 'puxada de nota'
 curso = open('cursos.txt', 'r')
-cursoid = curso.readlines()
-contadorc = 2  # padrão (1)
+cursoId = curso.readlines()
+contadorCurso = 0  # padrão (0)
+contadorTurma = 0  # padrão (0)
+contadorDisciplina = 0  # padrão (0)
 
+# verificar bkp e setar contadores posição de onde parou ou pular
+pass
 
-for x in cursoid:
-    elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="curso_chosen"]/a')))
-    elemento.click()
-    time.sleep(1)
-    navegador.find_element('xpath', '//*[@id="curso_chosen"]/div/div/input').send_keys(cursoid[contadorc])
-    time.sleep(1)
-    # extração do texto do elemento turmas
-    elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="turma_chosen"]/a')))
-    elemento.click()
-    time.sleep(1)
-    elemento = navegador.find_element('xpath', '//*[@id="turma_chosen"]/div/ul').get_attribute("innerText")
-    b = open('turmas.txt', 'w')
-    b.write(elemento)
-    b.close()
-    time.sleep(1)
-    turma = open('turmas.txt', 'r')
-    turmaid = turma.readlines()
-    # inserir lógica para remoção das sentenças que denominam se a turma é ativa ou não
-    pass
-    contadort = 1  # padrão (1)
-    if contadorc <= len(cursoid):
-        contadorc = contadorc + 1
-    else:
-        break
-    for y in turmaid:
+for x in cursoId:
+    if contadorCurso < len(cursoId):
+        contadorCurso = contadorCurso + 1
+        elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="curso_chosen"]/a')))
+        elemento.click()
+        time.sleep(1)
+        elemento = navegador.find_element('xpath', '//*[@id="curso_chosen"]/div/div/input')
+        elemento.send_keys(cursoId[contadorCurso])
+        time.sleep(1)
+        elemento.send_keys(Keys.ENTER)
+        time.sleep(1)
+        # extração do texto do elemento turmas
         elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="turma_chosen"]/a')))
         elemento.click()
         time.sleep(1)
-        navegador.find_element('xpath', '//*[@id="turma_chosen"]/div/div/input').send_keys(turmaid[contadort])
-        # extração do texto do elemento disciplinas
-        elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="disciplina_chosen"]')))
-        elemento.click()
+        elemento = navegador.find_element('xpath', '//*[@id="turma_chosen"]/div/ul').get_attribute("innerText")
+        b = open('turmas.txt', 'w')
+        b.write(elemento)
+        b.close()
         time.sleep(1)
-        elemento = navegador.find_element('xpath', '//*[@id="disciplina_chosen"]/div/ul'). \
-            get_attribute("innerText")
-        c = open('disciplinas.txt', 'w')
-        c.write(elemento)
-        c.close()
-        time.sleep(1)
-        disc = open('disciplinas.txt', 'r')
-        discid = disc.readlines()
-        contadord = -1  # padrão (-1)
-        if contadort <= len(turmaid):
-            contadort = contadort + 1
+        turma = open('turmas.txt', 'r')
+        turmaId = turma.readlines()
+    else:
+        break
+    for y in turmaId:
+        if contadorTurma < len(turmaId):
+            contadorTurma = contadorTurma + 1
+            elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="turma_chosen"]/a')))
+            elemento.click()
+            time.sleep(1)
+            elemento = navegador.find_element('xpath', '//*[@id="turma_chosen"]/div/div/input')
+            elemento.send_keys(turmaId[contadorTurma])
+            time.sleep(1)
+            elemento.send_keys(Keys.ENTER)
+            time.sleep(1)
+            # extração do texto do elemento disciplinas
+            elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="disciplina_chosen"]')))
+            elemento.click()
+            time.sleep(1)
+            elemento = navegador.find_element('xpath', '//*[@id="disciplina_chosen"]/div/ul'). \
+                get_attribute("innerText")
+            c = open('disciplinas.txt', 'w')
+            c.write(elemento)
+            c.close()
+            time.sleep(1)
+            disc = open('disciplinas.txt', 'r')
+            discId = disc.readlines()
         else:
+            contadorTurma = 0
             break
-        for z in discid:
-            if contadord <= len(discid):
-                contadord = contadord + 1
+        for z in discId:
+            if contadorDisciplina < len(discId):
                 elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="disciplina_chosen"]')))
                 elemento.click()
                 time.sleep(1)
                 elemento.click()
                 time.sleep(1)
-                navegador.find_element('xpath', '//*[@id="disciplina_chosen"]/div/div/input'). \
-                    send_keys(discid[contadord])
-                # botão carregar nota
+                elemento = navegador.find_element('xpath', '//*[@id="disciplina_chosen"]/div/div/input')
+                elemento.send_keys(discId[contadorDisciplina])
+                time.sleep(1)
+                elemento.send_keys(Keys.ENTER)  
+                time.sleep(1)
+                # botão carregar médias
                 elemento = wait.until(ec.element_to_be_clickable(navegador.
                                                                  find_element('xpath', '//*[''@id''="carregarNotas"]')))
                 elemento.click()
@@ -115,6 +126,7 @@ for x in cursoid:
                 elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="btnLancado"]')))
                 elemento.click()
                 # conferir todas as checkbox para deixá-las marcadas, usar navegador.find_element().is_selected()
+                pass
                 # save btn
                 elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="noprint"]/button')))
                 elemento.click()
@@ -126,7 +138,9 @@ for x in cursoid:
                 elemento = wait.until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[6]/div/div/a')))
                 elemento.click()
                 time.sleep(1)
+                contadorDisciplina = contadorDisciplina + 1
             else:
+                contadorDisciplina = 0
                 break
 
 time.sleep(5)
